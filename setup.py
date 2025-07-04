5187b43aa70e78318484c66a0cd0f03ae7359243
niraj <nirajparadva2004@gmail.com>
2025-07-04 23:55:02 +0530

    Revert package version to 1.0.0 and clean up dist files.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name='git-agent',
-    version='1.4.1',
+    version='1.0.0',
     packages=find_packages(),
     install_requires=[
         "dotenv",
